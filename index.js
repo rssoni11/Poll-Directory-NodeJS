96067771fe5663b4c982ec1e86344dae6b0af8ea
Nagendra Sariya <nagendra.sariya@milestogo.tv>
2022-07-22 14:34:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+ async function checkFileExist() {
+    try {
+        await fs.promises.access(`${__dirname}/epa_1986.pdf`);
+        // The check succeeded
+        console.log("File present");
+    } catch (error) {
+        console.log("File not found continue polling again.")
+        // The check failed
+    }
+}
+
+var minutes = 1, the_interval = minutes * 60 * 1000;
+setInterval(function() {
+    checkFileExist();
+  // do your stuff here
+}, the_interval);
+
+(async () => {
+    await checkFileExist();
+})();
